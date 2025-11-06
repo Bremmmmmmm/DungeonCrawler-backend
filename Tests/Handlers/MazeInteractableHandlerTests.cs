385a005533684b97a4d41f87b7bfebe017273360
Bram Schouten <459133@student.fontys.nl>
2025-11-06 13:14:34 +0100

    Adds item retrieval by area functionality
    Adds tests for retrieving items by area and implements
    a mock item data access layer to facilitate testing.
    
    This ensures that items can be retrieved based on the
    area they are located in, which is crucial for game logic.

--- Tests/Handlers/MazeInteractableHandlerTests.cs
+++ Tests/Handlers/MazeInteractableHandlerTests.cs
@@ -1,4 +1,5 @@
 using Interface.Interfaces.Logic;
+using Interface.Dtos;
 using Logic.Factories;
 using Logic.Handlers;
 using Tests.Mocks;
@@ -21,20 +22,34 @@ public class MazeInteractableHandlerTests
     [TestMethod]
     public void MazeInteractableHandler_Constructor_Test()
     {
-        // Arrange
         var mazeInteractableHandler = new MazeInteractableHandler(_mazeInteractableContainer);
-        // Assert
         Assert.IsNotNull(mazeInteractableHandler);
     }
 
     [TestMethod]
-    public void MazeInteractableHandler_getEnemy_Test()
+    public async Task MazeInteractableHandler_getEnemy_Test()
     {
-        // Arrange
         var mazeInteractableHandler = new MazeInteractableHandler(_mazeInteractableContainer);
-        // Act
-        var enemy = mazeInteractableHandler.getEnemy();
-        // Assert
+        var enemy = await mazeInteractableHandler.getEnemy();
         Assert.IsNotNull(enemy);
+        Assert.IsInstanceOfType(enemy, typeof(EnemyDto));
+    }
+
+    [TestMethod]
+    public async Task MazeInteractableHandler_getItemInArea_Test()
+    {
+        var mazeInteractableHandler = new MazeInteractableHandler(_mazeInteractableContainer);
+        var item = await mazeInteractableHandler.getItemInArea(1);
+
+        // item can be null if no items exist for the area in the mock repository.
+        if (item != null)
+        {
+            Assert.IsInstanceOfType(item, typeof(ItemDto));
+            Assert.IsFalse(string.IsNullOrEmpty(item.name));
+        }
+        else
+        {
+            Assert.IsNull(item);
+        }
     }
 }
--- Tests/Mocks/MockItemDal.cs
+++ Tests/Mocks/MockItemDal.cs
@@ -5,8 +5,27 @@ namespace Tests.Mocks;
 
 public class MockItemDal : IItemDal
 {
+    public IEnumerable<ItemDto> _itemDtos = new List<ItemDto>
+    {
+        new()
+        {
+            name = "poke-ball",
+            displayName = "Item 1",
+            areaNumbers = new[] { 1 },
+            dropChance = 70
+        },
+        new()
+        {
+            name = "poke-ball",
+            displayName = "Item 2",
+            areaNumbers = new[] { 2 },
+            dropChance = 70
+        },
+    };
+
     public Task<IEnumerable<ItemDto>> GetItemsByArea(int areaId)
     {
-        throw new NotImplementedException();
+        var items = _itemDtos.Where(i => i.areaNumbers.Contains(areaId));
+        return Task.FromResult(items);
     }
 }
